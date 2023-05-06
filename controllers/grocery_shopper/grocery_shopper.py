bd9204ad034cddf904350afd75c75374a7aa1868
kimccarthy <kimccarthy51@gmail.com>
2023-05-06 13:29:07 -0600

    update May 6

--- controllers/grocery_shopper/grocery_shopper.py
+++ controllers/grocery_shopper/grocery_shopper.py
@@ -1,13 +1,28 @@
 """grocery controller."""
+#mapping
 
-# Nov 2, 2022
+#
+
+#finding
+#drive to right spot, turn to 0.
+#go slowly to waypoint (arm in front)
+#full stop at waypoint
+#if color detection says its up, reach up
+#arm out, then up
+#after grabbed, put in basket
+#arm in front again, move. 
+
+
+
 
 from controller import Robot
 from controller import Display
 from controller import Camera
+from controller import Keyboard
 import math
 import numpy as np
 import copy
+
 
 #Initialization
 print("=== Initializing Grocery Shopper...")
@@ -24,12 +39,18 @@ LIDAR_ANGLE_RANGE = math.radians(240)
 BEAR_TOLERANCE = 0.05
 SCALE = 10
 OFFSET = 180
+STOP_TOLERANCE = 0.0001
+STOP_NUMBER = 80
 # create the Robot instance.
 robot = Robot()
 #display = Display("camera")
 
+
 # get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
+
+keyboard = robot.getKeyboard()
+keyboard.enable(timestep)
 
 # The Tiago robot has multiple motors, each identified by their names below
 part_names = ("head_2_joint", "head_1_joint", "torso_lift_joint", "arm_1_joint",
@@ -73,6 +94,7 @@ lidar.enablePointCloud()
 
 # Enable display
 display = robot.getDevice("display")
+
 #display.height = camera.getHeight()
 #display.width = camera.getWidth()
 #display.enable(timestep)
@@ -85,11 +107,13 @@ pose_theta = 0
 vL = 0
 vR = 0
 
+
+
 lidar_sensor_readings = [] # List to hold sensor readings
 lidar_offsets = np.linspace(-LIDAR_ANGLE_RANGE/2., +LIDAR_ANGLE_RANGE/2., LIDAR_ANGLE_BINS)
 lidar_offsets = lidar_offsets[83:len(lidar_offsets)-83] # Only keep lidar readings not blocked by robot chassis
 
-map = None
+map = np.zeros(shape=[360,360])
 robot_parts["wheel_left_joint"].getPositionSensor().enable(timestep)
 robot_parts["wheel_right_joint"].getPositionSensor().enable(timestep)
 
@@ -99,7 +123,16 @@ top_shelf = [1.6, -0.18, -3.2, 1, -1.9, -1, 0]
 overhead = [2.68, 1.02, 0, -0.32, 0, 0, 0]
 basket = [1.6, 1.02, 0, 2.29, -2.07, 1.39, 2.07] 
 
-yellow_range = [[210, 210, 0], [255, 255, 30]] #RGB, might be an issue
+#LOW: arm_grab_left = [2.68, -0.18, -1, -0.32, 2.07, 0.7, 2] 
+arm_grab_left_high = [2.68, 0.3, -1, -0.32, -2.07, 1.2, 2.07]
+arm_grab_left_low = [2.68, -0.4, -1, -0.32, -2.07, 1.2, 2.07] 
+arm_out = [1.3, -0.2, 0, -0.2, 0, 0, 0]
+arms = [overhead, arm_grab_left_high, arm_grab_left_low, basket]
+
+get_state = "move"
+state_counter = 0 
+
+yellow_range = [[210, 210, 0], [255, 255, 30]]
 MASK_WIDTH = 240
 MASK_HEIGHT = 135
 
@@ -107,7 +140,13 @@ robots_path = []
 bounding_box_x = [0,0,240,240]
 bounding_box_y = [0,135,135,0]
 
-waypoints = [[0, 5.4],[8, 5.6], [16.5,5.8], [16.5, 2.5], [12, 2.2], [0, 2.0]]
+
+mode = 'amanual'
+
+#waypoints = [[0, 5.4],[8, 5.6], [16.5,5.8], [16.5, 2.5], [12, 2.2], [0, 2.0]]
+
+up_v_down = [-1, 1, 0, 1, 0, 1, 0] #go, turn, then grabbing each. 
+waypoints = [[0, 2.4], [1.1, 2.6], [2, 2.6], [7, 2.6]]
 display.setColor(0x00FF00)
 for w in waypoints:
     display.drawPixel(w[0]*SCALE+OFFSET, w[1]*SCALE+OFFSET)
@@ -247,12 +286,130 @@ test_poseY = 0
 #~~~~~~START LOOP~~~~~~~~~~
 stop_count = 0
 while robot.step(timestep) != -1:
-    #
-   
+    #pose_x = gps.getValues()[0]
+    #pose_y = gps.getValues()[1]
     
+    robot_parts["torso_lift_joint"].setPosition(0.35)
+    
+    #n = compass.getValues()
+    #rad = -((math.atan2(n[0], n[1]))-1.5708)
+    #pose_theta = rad
+    
+    lidar_sensor_readings = lidar.getRangeImage()
+    lidar_sensor_readings = lidar_sensor_readings[83:len(lidar_sensor_readings)-83]
+
+    for i, rho in enumerate(lidar_sensor_readings):
+        alpha = lidar_offsets[i]
+
+        if rho > LIDAR_SENSOR_MAX_RANGE:
+            continue
+
+        # The Webots coordinate system doesn't match the robot-centric axes we're used to
+        rx = math.cos(alpha)*rho
+        ry = -math.sin(alpha)*rho
+
+        t = pose_theta + np.pi/2.
+        # Convert detection from robot coordinates into world coordinates
+        wx =  math.cos(t)*rx - math.sin(t)*ry + pose_x
+        wy =  math.sin(t)*rx + math.cos(t)*ry + pose_y
+        
+        mx = 360-abs(int((5+pose_x)*25))
+        my = 117-abs(int(pose_y*15.66))
+        
+        if rho < LIDAR_SENSOR_MAX_RANGE:
+            map_i = 360-abs(int((5+pose_x)*25)) #mx
+            map_j = 117-abs(int(pose_y*15.66))  #my
+            if (map_i >= 360):
+                map_i = 359
+            if (map_j >= 360):
+                map_j = 359
+            if (map_i < 0):
+                map_i = 0
+            if (map_j < 0):
+                map_j = 0
+            # Part 1.3: visualize map gray values.
+            # Only mapping when moving
+            if not(vR and vL == 0):
+                map[map_j][map_i] += 0.004
+                g = map[map_j][map_i]
+                if (g > 1):
+                    g = 1
+                color = (g*256**2+g*256+g)*255
+                display.setColor(int(color))
+                display.drawPixel(map_j, map_i)
+                
+    display.setColor(int(0xFF0000))
+    display.drawPixel(my,mx)
+    print(map_j,map_i)
+    """             
+    if mode == 'manual':
+        key = keyboard.getKey()
+        while(keyboard.getKey() != -1): pass
+        if key == keyboard.LEFT :
+            vL = -MAX_SPEED
+            vR = MAX_SPEED
+        elif key == keyboard.RIGHT:
+            vL = MAX_SPEED
+            vR = -MAX_SPEED
+        elif key == keyboard.UP:
+            vL = MAX_SPEED
+            vR = MAX_SPEED
+        elif key == keyboard.DOWN:
+            vL = -MAX_SPEED
+            vR = -MAX_SPEED
+        elif key == ord(' '):
+            vL = 0
+            vR = 0
+        elif key == ord('S'):
+            # Part 1.4: Filter map and save to filesystem
+            filter_map = map > 0.7
+            ones = np.ones(shape=[360,235])
+            filter_map = np.multiply(filter_map, ones)
+            for i in range(360):
+                for j in range(360):
+                    if (filter_map[i][j] == 0):
+                        display.setColor(int(0x000000))
+                        display.drawPixel(i, j)
+            np.save('map', filter_map)
+            print("Map file saved")
+        elif key == ord('L'):
+            # You will not use this portion in Part 1 but here's an example for loading saved a numpy array
+            load_map = np.load("map.npy")
+            for i in range(360):
+               for j in range(360):
+                   if (load_map[i][j] == 0):
+                       display.setColor(int(0x000000))
+                       display.drawPixel(i, j)
+                   else:
+                       display.setColor(int(0xFFFFFF))
+                       display.drawPixel(i, j)
+                       map[i][j] = 1
+            print("Map loaded")
+        else: # slow down
+            vL *= 0.75
+            vR *= 0.75
+            
+        #display.setColor(0xFFFFFF)   # White lines for empty space
+        #display.drawLine(int(pose_x*SCALE)+OFFSET, int(pose_y*SCALE)+OFFSET, int(wx*SCALE)+OFFSET, int(wy*SCALE)+OFFSET)
+    
+    #~~~~~~~LIDAR~~~~~~~~
+    if robot_state=="planning":  
+        for ray in lidar_readings:
+            display.setColor(0xFFFFFF)   # White lines for empty space
+            display.drawLine(int(pose_x*SCALE)+OFFSET, int(pose_y*SCALE)+OFFSET, int(ray[0]*SCALE)+OFFSET, int(ray[1]*SCALE)+OFFSET)
+            display.setColor(0xFF0000)   # Red points for robot path
+            display.drawPixel(int(pose_y*SCALE)*OFFSET, int(pose_x*SCALE)+OFFSET)
+            display.setColor(0x0000FF)   # Blue points for obstacle edges
+            display.drawPixel(int(ray[0]*SCALE)+OFFSET, int(ray[1]*SCALE)+OFFSET)
+    """ 
     # Read Lidar           
     lidar_sensor_readings = lidar.getRangeImage()
     lidar_readings = []
+    
+    
+    """
+    ###LIDAR READINGS
+    
     
     display.setColor(0xFF0000)
     display.drawPixel(int(pose_x*SCALE)+OFFSET, int(pose_y*SCALE)+OFFSET)
@@ -273,29 +430,30 @@ while robot.step(timestep) != -1:
     
     ##### Part 4: Draw the obstacle and free space pixels on the map
     robots_path.append([pose_x, pose_y])   # Keeping track of all points on robot path
+    """
     
-    for ray in lidar_readings:
-        display.setColor(0xFFFFFF)   # White lines for empty space
-        display.drawLine(int(pose_x*SCALE)+OFFSET, int(pose_y*SCALE)+OFFSET, int(ray[0]*SCALE)+OFFSET, int(ray[1]*SCALE)+OFFSET)
-        #print("LIDAR", int(pose_y*SCALE)+OFFSET, int(pose_x*SCALE)+OFFSET, int(ray[1]*SCALE)+OFFSET, int(ray[0]*SCALE)+OFFSET)
-        display.setColor(0xFF0000)   # Red points for robot path
-        #display.drawPixel(int(pose_y*SCALE)*OFFSET, int(pose_x*SCALE)+OFFSET)
-        #display.setColor(0x0000FF)   # Blue points for obstacle edges
-        #display.drawPixel(int(ray[0]*SCALE)+OFFSET, int(ray[1]*SCALE)+OFFSET)
-    display.setColor(0xFF00FF) 
-    for point in robots_path:
-        display.drawPixel(pose_x, pose_y)
-   
+    
+
+       
     #color blob detection
     #color_handler()
-    curr_positionL = robot_parts["wheel_left_joint"].getPositionSensor().getValue()
+    
+    #******ODOMETRY*******
+    
+    #Left position, taken from position sensors for accuracy
+    curr_positionL = robot_parts["wheel_left_joint"].getPositionSensor().getValue() 
     changeL = curr_positionL-prev_positionL
     lin_velL = ((changeL)/(timestep/1000.0))
     
+    #Right positions
     curr_positionR = robot_parts["wheel_right_joint"].getPositionSensor().getValue()
     changeR = curr_positionR-prev_positionR
     lin_velR = ((changeR)/(timestep/1000.0))
     
+    prev_positionL = curr_positionL
+    prev_positionR = curr_positionR
+    
+    #Odometry 
     distL = lin_velL/MAX_SPEED * MAX_SPEED_MS * timestep/1000.0
     distR = lin_velR/MAX_SPEED * MAX_SPEED_MS * timestep/1000.0
     pose_x += (distL+distR) / 2.0 * math.cos(pose_theta)
@@ -303,7 +461,10 @@ while robot.step(timestep) != -1:
     pose_theta += (distR-distL)/AXLE_LENGTH
     pose_theta = pose_theta%(2*np.pi)
     
+    
     #move based on state
+    
+    """
     if(dir_status=="stopped" and stop_count>0):
         vL = 0
         vR = 0
@@ -311,33 +472,41 @@ while robot.step(timestep) != -1:
         print(stop_count)
     elif(dir_status=="stopped"):
         dir_status = "drive"
-    elif(dir_status=="drive" and stop_count==0):
-        print("drive")
-        vL = MAX_SPEED/2
-        vR = MAX_SPEED/2
+    """
+    #Drive, left, right
+    if(dir_status=="drive"):# and stop_count==0):
+        #print("drive")
+        vL = MAX_SPEED/4
+        vR = MAX_SPEED/4
     elif(dir_status=="left"):
         vL = -MAX_SPEED/8
         vR = MAX_SPEED/8
     elif(dir_status=="right"):
         vL = MAX_SPEED/8
         vR = -MAX_SPEED/8
+    elif(dir_status=="stopped"):
+        vL = 0
+        vR = 0
     else:
         vL = 0
         vR = 0
         print("INVALID STATE: STOPPING")
-        
-    prev_positionL = curr_positionL
-    prev_positionR = curr_positionR
     
+    
+    
+    
+    #Bearing calculation
     ydist = waypoints[curr][1]-pose_y
     xdist = waypoints[curr][0]-pose_x
     theta = np.arctan2(ydist,xdist)
     print("THETAS", theta, pose_theta, theta-pose_theta, "DISTS", ydist, xdist)
-
+    
+    #Errors
     bear = angle_check(theta, pose_theta) 
     dist = np.sqrt(ydist**2 + xdist**2)
     display.setColor(0xFF0000)
-
+    
+    #which way to turn based off of bearing
     if((bear>BEAR_TOLERANCE and bear<(2*np.pi - BEAR_TOLERANCE)) or (bear<-BEAR_TOLERANCE and bear>-(2*np.pi - BEAR_TOLERANCE)) and stop_count==0):
         if(bear>0):
             dir_status = "left"
@@ -345,46 +514,134 @@ while robot.step(timestep) != -1:
             dir_status = "right"
     else:
         dir_status = "drive"
-        
-    gV = robot_parts["wheel_left_joint"].getVelocity()
-    gV2 = robot_parts["wheel_right_joint"].getVelocity()
-    print("B:", bear, "D:", dist, "VL,Real", lin_velL, lin_velR, "POSEX/Y:", pose_x, pose_y)
-    if(dist<0.1):
-        curr+=1
+    
+    #gV = robot_parts["wheel_left_joint"].getVelocity()
+    #gV2 = robot_parts["wheel_right_joint"].getVelocity()
+    print("B:", bear, "D:", dist, "VL,Real", lin_velL, lin_velR, "POSEX/Y:", pose_x, pose_y, vL, vR)
+    
+    #move to next waypoint (for now, until arm movement) 
+    
+       
     counter = counter + 1
-    if(dir_status=="arm"):
-        for joint in range(len(arm_joint)):
-            robot_parts[arm_joint[joint]].setPosition(arm_statuses[arm_status][joint])
+    #if(dir_status=="arm"):
+    #    for joint in range(len(arm_joint)):
+    #        robot_parts[arm_joint[joint]].setPosition(arm_statuses[arm_status][joint])
     #print(counter)
         #print(arm_statuses[arm_status][joint])
+     
+     
+    #*****GRAB SOMETHING STATE***** 
+    #stop    
     
-    if(vL==vR): #accounting for robot error, making it so that one wheel doesn't go insance
+    if get_state=="stop":
+        vL = 0
+        vR = 0
+        if(lin_velL<STOP_TOLERANCE and lin_velR<STOP_TOLERANCE):
+            #if -1, then keep going
+            get_state="move_arm_out"
+            state_counter = 0
+            
+    #Move arm out to not hit a shelf when moving arm up or down        
+    elif get_state=="move_arm_out":
+        vL = 0
+        vR = 0
+        for i in range(len(arm_joint)):
+            robot_parts[arm_joint[i]].setPosition(arm_out[i])
+        state_counter = state_counter+1
+        if state_counter>=STOP_NUMBER:
+            get_state = "move_arm_to_shelf"
+            state_counter = 0
+            
+    #Put arm to a given shelf        
+    elif get_state=="move_arm_to_shelf":
+        vL = 0
+        vR = 0
+        up = False
+        if up_v_down[curr-1] == 1:
+            up = True
+        for i in range(len(arm_joint)):
+            if up:
+                robot_parts[arm_joint[i]].setPosition(arm_grab_left_high[i])
+                robot_parts["torso_lift_joint"].setPosition(0.35)
+            else:
+                robot_parts[arm_joint[i]].setPosition(arm_grab_left_low[i])
+                robot_parts["torso_lift_joint"].setPosition(0)
+        state_counter = state_counter+1
+        if state_counter>=STOP_NUMBER:
+            get_state = "grab"
+            state_counter = 0
+    
+    #Grab with hand        
+    elif get_state == "grab":
+        vL = 0
+        vR = 0
+        robot_parts["gripper_left_finger_joint"].setPosition(0.045)
+        robot_parts["gripper_right_finger_joint"].setPosition(0.045)
+        state_counter = state_counter+1
+        if state_counter>=STOP_NUMBER:
+            get_state = "basket"
+            state_counter = 0
+            
+    #Move arm to basket
+    elif get_state == "basket":
+        vL = 0
+        vR = 0
+        for i in range(len(arm_joint)):
+            robot_parts[arm_joint[i]].setPosition(basket[i])
+        state_counter = state_counter+1
+        if state_counter>=STOP_NUMBER:
+            get_state = "drop"
+            state_counter = 0
+            
+    elif get_state == "drop":
+        vL = 0
+        vR = 0
+        robot_parts["gripper_left_finger_joint"].setPosition(0.045)
+        robot_parts["gripper_right_finger_joint"].setPosition(0.045)
+        state_counter = state_counter+1
+        if state_counter>=STOP_NUMBER:
+            get_state = "move"
+            state_counter = 0
+    print(get_state, state_counter)    
+          
+    
+            
+    #Error compensation
+    if(vL==vR and vL!=0): #accounting for robot error, making it so that one wheel doesn't go insanse
         if(lin_velL<lin_velR - TOLERANCE):
             vR = np.abs(lin_velL) + 0.1
         elif(lin_velR<lin_velL - TOLERANCE):
            vL = np.abs(lin_velR) + 0.1
-    print(vL, vR, gV, gV2)
+    #print(vL, vR, gV, gV2)
+    
+    #********ARM CONTROL HERE*****
+    
+    #for i in range(len(arm_joint)):
+    #    robot_parts[arm_joint[i]].setPosition(arm_grab_left[i])
+    #***CHECK ACCELERATION***
+    if(dist<0.1):
+        curr+=1
+        if(up_v_down[curr-1]>=0):
+            get_state = "stop"
+    
     robot_parts["wheel_left_joint"].setVelocity(vL)
     robot_parts["wheel_right_joint"].setVelocity(vR)
     
-    #***CHECK ACCELERATION***
-    
-    
-    
     #print(right_gripper_enc.getValue(), left_gripper_enc.getValue(), gripper_status)
-    
+    """
     if(gripper_status=="open"):
         # Close gripper, note that this takes multiple time steps...
         robot_parts["gripper_left_finger_joint"].setPosition(0)
         robot_parts["gripper_right_finger_joint"].setPosition(0)
-        if right_gripper_enc.getValue()<=0.005:
-            gripper_status="closed"
+        #if right_gripper_enc.getValue()<=0.005:
+        #    gripper_status="closed"
     else:
         # Open gripper
         robot_parts["gripper_left_finger_joint"].setPosition(0.045)
         robot_parts["gripper_right_finger_joint"].setPosition(0.045)
-        if left_gripper_enc.getValue()>=0.044:
-            gripper_status="open"
+        #if left_gripper_enc.getValue()>=0.044:
+        #    gripper_status="open"
+    """
     #display.drawPixel(pose_x*SCALE+130, pose_y*SCALE+130)        
     #only for blob map
     #count = (count + 1)%10
